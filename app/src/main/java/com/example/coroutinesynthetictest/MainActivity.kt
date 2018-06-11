b8b5db0ab1b2a8b8aaf45e478543c49c4f69256b
bit <bit@cinemood.com>
2018-06-11 22:57:44 +0300

    coroutine test

--- app/src/main/java/com/example/coroutinesynthetictest/MainActivity.kt
+++ app/src/main/java/com/example/coroutinesynthetictest/MainActivity.kt
@@ -2,11 +2,50 @@ package com.example.coroutinesynthetictest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log.d
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.launch
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var list: MutableList<Int>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        for (i in 0..1000) {
+            list.add(i + Random().nextInt(10000))
+        }
+    }
+
+    private fun sortList() {
+        for (i in 0..100) {
+            list.sort()
+        }
+    }
+
+    private fun d(log: String) {
+        d("TagSyntheticTest", log)
+    }
+
+    private fun sortWithCoroutine() {
+        val startTime = Date().time
+        d("coroutine start")
+        launch(UI) {
+            val result = async { sortList() }.await()
+            d("result = $result")
+            checkTime(startTime, result)
+        }
+    }
+
+    private fun checkTime(startTime: Long, result: Unit) {
+        val resultTime = Date().time - startTime
+        d("result")
+        d("finish\n it takes$resultTime millis")
+        textView.text = resultTime.toString()
     }
 }
